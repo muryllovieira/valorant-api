e8d079bdf100e5412a4c25ed41eb6da704fd3468
Muryllo Vieira <muryllovieira59@gmail.com>
2023-05-05 13:21:39 -0300

    falta o carousel e a responsividade

--- js/agentes.js
+++ js/agentes.js
@@ -5,6 +5,16 @@ const agente = await showAgentes()
 
 console.log(agente);
 
+function deleteAndCreateCard(agente1) {
+    // remove o elemento no indice 8
+    agente1.splice(8, 1);
+
+    // chama a função de criar o card de agente com a array modificada
+    return agente1
+}
+
+deleteAndCreateCard(agente)
+
 const createAgentes = (agente1) => {
 
     const containerCard = document.createElement('div')
@@ -27,26 +37,18 @@ const createAgentes = (agente1) => {
 
     const classAgente = document.createElement('p')
     classAgente.classList.add('classAgente')
-    classAgente.textContent = agente1.role
+    classAgente.textContent = agente1.role.displayName
 
     console.log(classAgente);
 
     containerCard.append(cardAgente)
-    cardAgente.append(imgAgente, nameAgente, classAgente)
+    cardAgente.append(nameAgente, imgAgente, classAgente)
 
     return containerCard
 
 }
 
-function deleteAndCreateCard(agente1) {
-    // remove o elemento no indice 8
-    agente1.splice(8, 1);
 
-    // chama a função de criar o card de agente com a array modificada
-    createAgentes(agente1);
-}
-
-deleteAndCreateCard(agente)
 
 
 export const loadContainerAgentes = () => {
